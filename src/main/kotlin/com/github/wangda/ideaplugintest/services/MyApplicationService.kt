36fcce3ce9ad5bda5ae7b853e8226195a072883a
GitHub Action <action@github.com>
2022-05-21 08:58:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangda/ideaplugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wangda/ideaplugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangda.ideaplugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangda.ideaplugintest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/wangda/ideaplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wangda/ideaplugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangda.ideaplugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangda.ideaplugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
